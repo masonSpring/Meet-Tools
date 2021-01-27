c888bad17a2acadd66023640dff67c96417bd4e4
Noah Jacobson <noah@noahjacobson.com>
2021-01-27 12:47:20 -0500

    Prevent same caption triggering more than once

--- captions.js
+++ captions.js
@@ -11,63 +11,64 @@ chrome.storage.sync.get({
     haHook = items.haHook;
     ping = items.ping;
     if (webhook != "" || haHook != "") {
-        readCaption()
+        readCaption("")
     }
 });
 
-function readCaption() {
-    //generate caption text
+function readCaption(oldCaption) {
     let captionArr = document.getElementsByClassName('CNusmb');
     let caption = "";
     for (let x = 0; x < captionArr.length; x += 1) {
         caption += captionArr[x].innerText + " ";
     }
     shortloop = true;
-    captionLoop:
-    for (let y = 0; y < include.length; y += 1) {
-        if (caption.toLowerCase().includes(include[y])) {
-            shortloop = false;
+    if (caption != oldCaption) {
+        captionLoop:
+        for (let y = 0; y < include.length; y += 1) {
+            if (caption.toLowerCase().includes(include[y])) {
+                shortloop = false;
 
-            if (exclude != "") {
-                for (let z = 0; z < exclude.length; z += 1){
-                    if(caption.toLowerCase().includes(exclude[z])){
-                        setTimeout(readCaption, 1000);
-                        break captionLoop;
+                if (exclude != "") {
+                    for (let z = 0; z < exclude.length; z += 1){
+                        if(caption.toLowerCase().includes(exclude[z])){
+                            setTimeout(readCaption, 1000, oldCaption);
+                            break captionLoop;
+                        }
                     }
                 }
-            }
-            if (webhook != "") {
-                let request = new XMLHttpRequest();
-                request.open("POST", webhook);
-                request.setRequestHeader('Content-type', 'application/json');
+                 if (webhook != "") {
+                    let request = new XMLHttpRequest();
+                    request.open("POST", webhook);
+                    request.setRequestHeader('Content-type', 'application/json');
 
-                let captionEmbed = {
-                        "title": "Caption Text",
-                        "description": caption
-                };
+                    let captionEmbed = {
+                            "title": "Caption Text",
+                            "description": caption
+                    };
+   
+                    let params = {
+                        "content": "<@"+ping+">",
+                        embeds: [ captionEmbed ],
+                        username: document.getElementsByClassName('zs7s8d jxFHg')[document.getElementsByClassName('zs7s8d jxFHg').length - 1].innerText,
+                        avatar_url: document.getElementsByClassName('KpxDtd r6DyN')[document.getElementsByClassName('KpxDtd r6DyN').length - 1].src
+                    }
 
-                let params = {
-                    "content": "<@"+ping+">",
-                    embeds: [ captionEmbed ],
-                    username: document.getElementsByClassName('zs7s8d jxFHg')[document.getElementsByClassName('zs7s8d jxFHg').length - 1].innerText,
-                    avatar_url: document.getElementsByClassName('KpxDtd r6DyN')[document.getElementsByClassName('KpxDtd r6DyN').length - 1].src
+                    request.send(JSON.stringify(params));
                 }
-
-                request.send(JSON.stringify(params));
-            }
         
-            if (haHook != "") {
-                let request = new XMLHttpRequest();
-                request.open("POST", haHook);
-                request.send();
+                if (haHook != "") {
+                    let request = new XMLHttpRequest();
+                    request.open("POST", haHook);
+                    request.send();
+                }
+        
+                break captionLoop;
             }
-    
-            break forLoop;
         }
     }
     if(shortloop == true){
-        setTimeout(readCaption, 1000);
+        setTimeout(readCaption, 1000, caption);
     } else {
-        setTimeout(readCaption, 15000);
+        setTimeout(readCaption, 15000, caption);
     }
 }
